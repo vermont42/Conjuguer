8eac8e39a4fbdb21e62016d50d7a3083f7998692
vermont42 <vermontcoder@gmail.com>
2021-01-06 06:57:55 -0800

    remove duplication

--- Conjuguer/App/ConjuguerApp.swift
+++ Conjuguer/App/ConjuguerApp.swift
@@ -22,6 +22,8 @@ struct ConjuguerApp: App {
     print("verb count: \(Verb.verbs.count)  model count: \(VerbModel.models.count)\n")
 
     for verb in ["arriver", "colorer", "finir", "lancer", "parler"] {
+      var output = "\(verb) "
+
       let participe: String
       let participeResult = Conjugator.conjugate(infinitive: verb, tense: .participePassé, personNumber: nil)
       switch participeResult {
@@ -30,62 +32,18 @@ struct ConjuguerApp: App {
       default:
         fatalError()
       }
+      output += "\(participe) "
 
-      let je: String
-      let jeResult = Conjugator.conjugate(infinitive: verb, tense: .indicatifPrésent, personNumber: .firstSingular)
-      switch jeResult {
-      case .success(let value):
-        je = value
-      default:
-        fatalError()
+      for personNumber in [PersonNumber.firstSingular, .secondSingular, .thirdSingular, .firstPlural, .secondPlural, .thirdPlural] {
+        let présentResult = Conjugator.conjugate(infinitive: verb, tense: .indicatifPrésent, personNumber: personNumber)
+        switch présentResult {
+        case .success(let value):
+          output += "\(value) "
+        default:
+          fatalError()
+        }
       }
-
-      let tu: String
-      let tuResult = Conjugator.conjugate(infinitive: verb, tense: .indicatifPrésent, personNumber: .secondSingular)
-      switch tuResult {
-      case .success(let value):
-        tu = value
-      default:
-        fatalError()
-      }
-
-      let il: String
-      let ilResult = Conjugator.conjugate(infinitive: verb, tense: .indicatifPrésent, personNumber: .thirdSingular)
-      switch ilResult {
-      case .success(let value):
-        il = value
-      default:
-        fatalError()
-      }
-
-      let nous: String
-      let nousResult = Conjugator.conjugate(infinitive: verb, tense: .indicatifPrésent, personNumber: .firstPlural)
-      switch nousResult {
-      case .success(let value):
-        nous = value
-      default:
-        fatalError()
-      }
-
-      let vous: String
-      let vousResult = Conjugator.conjugate(infinitive: verb, tense: .indicatifPrésent, personNumber: .secondPlural)
-      switch vousResult {
-      case .success(let value):
-        vous = value
-      default:
-        fatalError()
-      }
-
-      let ils: String
-      let ilsResult = Conjugator.conjugate(infinitive: verb, tense: .indicatifPrésent, personNumber: .thirdPlural)
-      switch ilsResult {
-      case .success(let value):
-        ils = value
-      default:
-        fatalError()
-      }
-
-      print("\(verb) \(participe) \(je) \(tu) \(il) \(nous) \(vous) \(ils)\n")
+      print("\(output)\n")
     }
   }
 }
