933a42a694687dce08600722718b506007bf1856
vermont42 <vermontcoder@gmail.com>
2021-08-09 21:31:04 -0700

    add Infos

--- Conjuguer/Models/L.swift
+++ Conjuguer/Models/L.swift
@@ -129,6 +129,237 @@ enum L {
 """
     }
 
+    static var participePasséHeading: String {
+      NSLocalizedString("Info.participePasséHeading", comment: "")
+    }
+
+    static var participePasséText: String {
+      //NSLocalizedString("Info.participePasséText", comment: "")
+"""
+
+"""
+    }
+
+    static var participePrésentHeading: String {
+      NSLocalizedString("Info.participePrésentHeading", comment: "")
+    }
+
+    static var participePrésentText: String {
+      //NSLocalizedString("Info.participePrésentText", comment: "")
+"""
+
+"""
+    }
+
+    static var radicalFuturHeading: String {
+      NSLocalizedString("Info.radicalFuturHeading", comment: "")
+    }
+
+    static var radicalFuturText: String {
+      //NSLocalizedString("Info.radicalFuturText", comment: "")
+"""
+
+"""
+    }
+
+    static var indicatifPrésentHeading: String {
+      NSLocalizedString("Info.indicatifPrésentHeading", comment: "")
+    }
+
+    static var indicatifPrésentText: String {
+      //NSLocalizedString("Info.indicatifPrésentText", comment: "")
+"""
+
+"""
+    }
+
+    static var passéSimpleHeading: String {
+      NSLocalizedString("Info.passéSimpleHeading", comment: "")
+    }
+
+    static var passéSimpleText: String {
+      //NSLocalizedString("Info.passéSimpleText", comment: "")
+"""
+
+"""
+    }
+
+    static var imparfaitHeading: String {
+      NSLocalizedString("Info.imparfaitHeading", comment: "")
+    }
+
+    static var imparfaitText: String {
+      //NSLocalizedString("Info.imparfaitText", comment: "")
+"""
+
+"""
+    }
+
+    static var futurSimpleHeading: String {
+      NSLocalizedString("Info.futurSimpleHeading", comment: "")
+    }
+
+    static var futurSimpleText: String {
+      //NSLocalizedString("Info.futurSimpleText", comment: "")
+"""
+
+"""
+    }
+
+    static var conditionnelPrésentHeading: String {
+      NSLocalizedString("Info.conditionnelPrésentHeading", comment: "")
+    }
+
+    static var conditionnelPrésentText: String {
+      //NSLocalizedString("Info.conditionnelPrésentText", comment: "")
+"""
+
+"""
+    }
+
+    static var subjonctifPrésentHeading: String {
+      NSLocalizedString("Info.subjonctifPrésentHeading", comment: "")
+    }
+
+    static var subjonctifPrésentText: String {
+      //NSLocalizedString("Info.subjonctifPrésentText", comment: "")
+"""
+
+"""
+    }
+
+    static var subjonctifImparfaitHeading: String {
+      NSLocalizedString("Info.subjonctifImparfaitHeading", comment: "")
+    }
+
+    static var subjonctifImparfaitText: String {
+      //NSLocalizedString("Info.subjonctifImparfaitText", comment: "")
+"""
+
+"""
+    }
+
+    static var impératifHeading: String {
+      NSLocalizedString("Info.impératifHeading", comment: "")
+    }
+
+    static var impératifText: String {
+      //NSLocalizedString("Info.impératifText", comment: "")
+"""
+
+"""
+    }
+
+    static var passéComposéHeading: String {
+      NSLocalizedString("Info.passéComposéHeading", comment: "")
+    }
+
+    static var passéComposéText: String {
+      //NSLocalizedString("Info.passéComposéText", comment: "")
+"""
+
+"""
+    }
+
+    static var plusQueParfaitHeading: String {
+      NSLocalizedString("Info.plusQueParfaitHeading", comment: "")
+    }
+
+    static var plusQueParfaitText: String {
+      //NSLocalizedString("Info.plusQueParfaitText", comment: "")
+"""
+
+"""
+    }
+
+    static var passéAntérieurHeading: String {
+      NSLocalizedString("Info.passéAntérieurHeading", comment: "")
+    }
+
+    static var passéAntérieurText: String {
+      //NSLocalizedString("Info.passéAntérieurText", comment: "")
+"""
+
+"""
+    }
+
+    static var passéSurcomposéHeading: String {
+      NSLocalizedString("Info.passéSurcomposéHeading", comment: "")
+    }
+
+    static var passéSurcomposéText: String {
+      //NSLocalizedString("Info.passéSurcomposéText", comment: "")
+"""
+
+"""
+    }
+
+    static var futurAntérieurHeading: String {
+      NSLocalizedString("Info.futurAntérieurHeading", comment: "")
+    }
+
+    static var futurAntérieurText: String {
+      //NSLocalizedString("Info.futurAntérieurText", comment: "")
+"""
+
+"""
+    }
+
+    static var conditionnelPasséHeading: String {
+      NSLocalizedString("Info.conditionnelPasséHeading", comment: "")
+    }
+
+    static var conditionnelPasséText: String {
+      //NSLocalizedString("Info.conditionnelPasséText", comment: "")
+"""
+
+"""
+    }
+
+    static var subjonctifPasséHeading: String {
+      NSLocalizedString("Info.subjonctifPasséHeading", comment: "")
+    }
+
+    static var subjonctifPasséText: String {
+      //NSLocalizedString("Info.subjonctifPasséText", comment: "")
+"""
+
+"""
+    }
+
+    static var subjonctifPlusQueParfaitHeading: String {
+      NSLocalizedString("Info.subjonctifPlusQueParfaitHeading", comment: "")
+    }
+
+    static var subjonctifPlusQueParfaitText: String {
+      //NSLocalizedString("Info.subjonctifPlusQueParfaitText", comment: "")
+"""
+
+"""
+    }
+
+    static var impératifPasséHeading: String {
+      NSLocalizedString("Info.impératifPasséHeading", comment: "")
+    }
+
+    static var impératifPasséText: String {
+      //NSLocalizedString("Info.impératifPasséText", comment: "")
+"""
+
+"""
+    }
+
+    static var questionsAndResponsesHeading: String {
+      NSLocalizedString("Info.questionsAndResponsesHeading", comment: "")
+    }
+
+    static var questionsAndResponsesText: String {
+      //NSLocalizedString("Info.questionsAndResponsesText", comment: "")
+"""
+
+"""
+    }
+
     static var creditsHeading: String {
       NSLocalizedString("Info.creditsHeading", comment: "")
     }
