4cdbb05024442859e2541017bb00ec1dbd34892c
vermont42 <vermontcoder@gmail.com>
2021-08-15 14:47:49 -0700

    fix Dark Mode Info

--- Conjuguer/Views/TextView.swift
+++ Conjuguer/Views/TextView.swift
@@ -9,19 +9,17 @@ import SwiftUI
 import UIKit
 
 struct TextView: UIViewRepresentable {
-//  @Binding var text: NSAttributedString
   let text: NSAttributedString
 
   func makeUIView(context: Context) -> UITextView {
     let v = UITextView()
     v.isEditable = false
-    v.textColor = UIColor(Color.customForeground)
-    v.backgroundColor = UIColor(Color.customBackground)
     v.contentOffset = .zero
     return v
   }
 
   func updateUIView(_ uiView: UITextView, context: Context) {
+    uiView.backgroundColor = UIColor(Color.customBackground)
     uiView.attributedText = text
     uiView.contentOffset = .zero
   }
