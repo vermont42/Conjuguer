844e56e794511ddfddae54b1add9f3f4ff19c268
vermont42 <vermontcoder@gmail.com>
2022-01-02 21:56:18 -0800

    add plist encryption key

---
+++ Conjuguer/Models/ImageInfo.swift
@@ -0,0 +1,13 @@
+//
+//  ImageInfo.swift
+//  Conjuguer
+//
+//  Created by Joshua Adams on 1/2/22.
+//
+
+import Foundation
+
+struct ImageInfo {
+  let filename: String
+  let accessibilityLabel: String
+}
